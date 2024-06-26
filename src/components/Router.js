62dacf11f8515fbddf6e366d5af29db78a392ea7
kenzay <imdanya946@gmail.com>
2024-06-26 13:22:31 +0300

    delete for gh-page

--- src/components/Router.js
+++
@@ -1,8 +0,0 @@
-import useNavigation from "../hooks/use-navigation";
-export default function Router({ to, children }) {
-  const { currentPath } = useNavigation();
-  if (currentPath === to) {
-    return children;
-  }
-  return null;
-}
--- src/pages/MainPage.js
+++
@@ -1,9 +0,0 @@
-function MainPage() {
-  return (
-    <div className="main-page">
-      <h1>YOU'RE WELCOME TO MY SITE</h1>
-    </div>
-  );
-}
-
-export default MainPage;
